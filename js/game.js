53d6096f5ddfa0225c89c7c87edf6e054f5b1983
myrelle <myptbr10@gmail.com>
2022-10-16 19:40:45 +0100

    (memory game)

---
+++ js/game.js
@@ -0,0 +1,75 @@
+const grid = document.querySelector('.grid');
+
+
+const characters = [
+    'mickey-minnei',
+    'mickey-rs',
+    'margarida',
+    'pluto',
+    'minnie',
+    'mickey-color',
+    'turma-mickey',
+    'casa-mickey',
+    'goof',
+    'pato-donalds',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+let firstCard = '';
+let secondCard = '';
+
+
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal-card')){
+        return;
+    }
+
+    if (firstCard === ''){
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    
+    } else if (secondCard === ''){
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+
+
+}
+
+const createCard = (character) => {
+    
+    const card =createElement('div', 'card');
+    const front  =createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../img2/${character}.png')`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard);
+
+   return card;
+}
+
+const loadGame = () => {
+    const duplicateCharacters = [...characters, ...characters];
+
+    const shuffledArray = duplicateCharacters.sort( () => Math.random() - 0.5);
+
+    shuffledArray.forEach((character) => {
+       const card = createCard(character);
+        grid.appendChild(card);
+    });
+}
+
+loadGame();
